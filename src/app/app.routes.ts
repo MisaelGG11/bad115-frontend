c3c257dd6f5ae09ef8d86aee6d514cbcb838718c
MisaelGG11 <magomez@innovacion.gob.sv>
2024-03-05 08:46:57 -0600

    feat: add component view to routes file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { IndexComponent } from './views/index/index.component';
+import { LoginComponent } from './views/auth/login/login.component';
+import { SignupComponent } from './views/auth/signup/signup.component';
 
 export const routes: Routes = [
     // no layout views
     { path: "", component: IndexComponent },
+    { path: "login", component: LoginComponent },
+    { path: "registro", component: SignupComponent }
   ];
