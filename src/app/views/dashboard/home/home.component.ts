1f7b17dee36bad102a5fc13468c71ba4b8fdd2f1
MisaelGG11 <magomez@innovacion.gob.sv>
2024-03-20 14:03:57 -0600

    feat: add ngx-sonner lib for toaster

--- src/app/views/dashboard/home/home.component.ts
+++ src/app/views/dashboard/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { toast } from 'ngx-sonner';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class HomeComponent {
-
+  protected readonly toast = toast;
 }
