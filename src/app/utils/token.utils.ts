a4c46cb1591341d03f2a0480957e44d719acb26d
MisaelGG11 <90297880+MisaelGG11@users.noreply.github.com>
2024-04-02 04:40:38 -0600

    feat: add utils for jwt tokens

---
+++ src/app/utils/token.utils.ts
@@ -0,0 +1,20 @@
+import { jwtDecode } from 'jwt-decode';
+import { UserData } from '../interfaces/user'; 
+
+// Decodifica un token
+export function decoderToken(token: any): UserData {
+  return jwtDecode<UserData>(token);
+}
+
+// Verifica expiración de token
+export function hasExpiredToken(token: string) {
+  const { exp } = decoderToken(token);
+
+  const currentData = new Date().getTime();
+
+  if (exp <= currentData) {
+    return true;
+  }
+
+  return false;
+}
