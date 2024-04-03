84e3035eceb0abba6d3ba076ad93673ee81a2601
MisaelGG11 <magomez@innovacion.gob.sv>
2024-04-02 18:05:20 -0600

    feat: add custom input text component

---
+++ src/app/components/inputs/custom-input/custom-input.component.ts
@@ -0,0 +1,84 @@
+import { Component, Input, forwardRef } from '@angular/core';
+import { Form, FormControl, FormGroup, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { InputErrorsComponent } from '../input-errors/input-errors.component';
+
+@Component({
+  selector: 'app-custom-input',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, InputErrorsComponent],
+  templateUrl: './custom-input.component.html',
+  styles: ``,
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => CustomInputComponent),
+    multi: true
+  }]
+})
+export class CustomInputComponent  {
+
+  public value: string = '';
+  public changed: (value: string) => void = () => {};
+  public touched: () => void = () => {};
+  public isDisabled: boolean = false;
+
+  // Input properties
+  @Input() parentForm : FormGroup | null = null;
+  @Input() fieldName : string = '';
+  @Input() placeholder: string = '';
+  @Input() prependIcon: string | null = null;
+  @Input() endIcon: string | null = null;
+  @Input() type: string = 'text';
+  @Input() rounded: string = 'lg';
+  @Input() label: string = '';
+
+  // Internal state variable
+  showPassword = false;
+
+  constructor() { }
+
+  // Computed property for input class
+  get inputClass() {
+    const classes = ['input-group--field'];
+    if (this.prependIcon) classes.push('pl-12');
+    if (this.endIcon) classes.push('pr-12');
+    if (this.rounded) {
+      if (this.rounded === 'full') classes.push('rounded-full');
+      else classes.push(`rounded-${this.rounded}`);
+    }
+    if (!this.prependIcon && !this.endIcon) classes.push('px-6');
+    return classes.join(' ');
+  }
+
+  // Toggle password visibility
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+  public writeValue ( value: string ): void {
+		this.value = value;
+	}
+
+  get formField(): FormControl{
+    return this.parentForm?.get(this.fieldName) as FormControl;
+  }
+
+  public onChange ( event: Event ): void {
+		const value: string =
+			( <HTMLInputElement>event.target ).value;
+
+		this.changed( value );
+	}
+
+	public registerOnChange ( fn: any ): void {
+		this.changed = fn;
+	}
+
+	public registerOnTouched ( fn: any ): void {
+		this.touched = fn;
+	}
+
+	public setDisabledState ( isDisabled: boolean ): void {
+		this.isDisabled = isDisabled;
+	}
+
+}
---
+++ src/app/components/inputs/input-errors/input-errors.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-input-errors',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './input-errors.component.html',
+  styles: ``
+})
+export class InputErrorsComponent {
+  @Input()
+	public formField: FormControl = new FormControl();
+
+  @Input()
+  public fieldName: string = '';
+}
