eda699ea1b18c5a7543568f311b89cdc7fa6aabc
MisaelGG11 <magomez@innovacion.gob.sv>
2024-04-02 18:05:56 -0600

    feat: add custom input component integration in login component

--- src/app/views/auth/login/login.component.ts
+++ src/app/views/auth/login/login.component.ts
@@ -1,21 +1,23 @@
 import { Component, inject } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { RouterLink } from '@angular/router';
-import { FormGroup, FormBuilder, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink, Router } from '@angular/router';
+import { FormGroup, FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../../services/auth.service';
 import { login, logout } from '../../../store/auth.actions';
 import { Session } from '../../../interfaces/user';
+import { CustomInputComponent } from '../../../components/inputs/custom-input/custom-input.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, ReactiveFormsModule, CommonModule],
+  imports: [RouterLink, ReactiveFormsModule, CommonModule, CustomInputComponent],
   templateUrl: './login.component.html',
   styles: ``
 })
 export class LoginComponent {
+  private router = inject(Router);
   private store = inject(Store);
   private authService = inject(AuthService);
   session$: Observable<Session>;
@@ -23,8 +25,8 @@ export class LoginComponent {
 
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
-      email: new FormControl(''),
-      password: new FormControl('')
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required)
     });
     this.session$ = this.store.select('session')
   }
@@ -33,9 +35,13 @@ export class LoginComponent {
     this.store.dispatch(login())
   }
 
-  submit() {
-    const response = this.authService.login(this.form.value.email, this.form.value.password);
-    this.loginStore()
-    console.log(this.session$);
+  async submit() {
+    if(this.form.valid){
+      const response = await this.authService.login(this.form.value.email, this.form.value.password);
+      if(response.status === 201 || response.status === 200){
+        this.loginStore()
+        this.router.navigate(['/dashboard'])
+      }
+    }
   }
 }
