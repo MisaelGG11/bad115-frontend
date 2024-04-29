e8c6d0053e31153ed9114a6d97c0c72c43af6a60
fabio248 <fabioflores021@gmail.com>
2024-04-29 14:30:47 -0600

    refactor: change ifs for switch

--- src/app/config/network.service.ts
+++ src/app/config/network.service.ts
@@ -3,34 +3,43 @@ import { toast } from "ngx-sonner"
 import { environment } from "../../environments/environment.development";
 
 let config = {
-  baseURL: environment.api || 'http://127.0.0.1:8001/api',
+  baseURL: environment.api || 'http://127.0.0.1:5757/v1',
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
     Authorization: `Bearer ${localStorage.getItem("token")}`,
   },
 }
-type ObjetoConArreglosDeString = {
+type ObjectArrayStrings = {
   [key: string]: string[];
 };
 
 const instance = axios.create(config)
 
-function setNotification(toastConfig: any) {
-  if (toastConfig.type === 'success'){
-    return toast.success(toastConfig.message)
-  } else if(toastConfig.type === 'error'){
-    return toast.error(toastConfig.message)
-  } else if(toastConfig.type === 'warning'){
-    return toast.warning(toastConfig.message)
-  } else if(toastConfig.type === 'info'){
-    return toast.info(toastConfig.message)
-  } else if(toastConfig.type === 'description'){
+type ToastType = 'success' | 'error' | 'warning' | 'info' | 'description';
+type ToastConfig = {
+  type: ToastType;
+  summary: string;
+  message: string;
+  description?: string;
+};
+
+function setNotification(toastConfig: ToastConfig) {
+  switch (toastConfig.type) {
+    case 'success':
+      return toast.success(toastConfig.message);
+    case 'error':
+      return toast.error(toastConfig.message);
+    case 'warning':
+      return toast.warning(toastConfig.message);
+    case 'info':
+      return toast.info(toastConfig.message);
+    case 'description':
     return toast.message(toastConfig.message, {
       description: toastConfig.description
-    })
-  } else {
-    return toast.message(toastConfig.message)
+    });
+    default:
+      return toast.message(toastConfig.message);
   }
 }
 
@@ -63,59 +72,66 @@ instance.interceptors.response.use(
     return response
   },
   async (error) => {
-    let message = '' as string | ObjetoConArreglosDeString
+    let message = '' as string | ObjectArrayStrings
     let summary = ''
-    let type = 'error'
-    if (error.response.status === 0){
-      summary = 'Error de conexión'
-      message = 'No se pudo conectar con el servidor'
-    } else if (error.response.status === 300){
-      return Promise.reject(error)
-    } else if (error.response.status === 500){
-      summary = 'Error de servidor'
-      message = 'Error'
-    } else if (error.response.status === 401 && !localStorage.getItem('refresh_token')){
-      summary = 'Advertencia'
-      message = error.response.data.message ?? 'Acceso no autorizado'
-    } else if (error.response.status === 401 && localStorage.getItem('refresh_token')){
-      try {
-        console.log(localStorage.getItem('refresh_token'))
-        const response = await axios.post(`${environment.api}/auth/refresh-token`, { refresh_token: localStorage.getItem('refresh_token') })
-        localStorage.setItem('access_token', response.data.accesToken);
-        localStorage.setItem('refresh_token', response.data.refreshToken);
+    let type: ToastType = 'error'
 
-        const config = error.config;
-        config.headers.Authorization = `Bearer ${response.data.accessToken}`;
-        return axios.request(config);
-      } catch (error) {
-        localStorage.removeItem('access_token');
-        localStorage.removeItem('refresh_token');
-        localStorage.removeItem('person');
-        window.location.href = '/login'
+    switch (error.response.status) {
+      case 0:
+        summary = 'Error de conexión';
+        message = 'No se pudo conectar con el servidor';
+        break;
+      case 300:
         return Promise.reject(error);
+      case 500:
+        summary = 'Error de servidor';
+        message = 'Error';
+        break;
+      case 401:
+        if (!localStorage.getItem('refresh_token')) {
+          summary = 'Advertencia';
+          message = error.response.data.message ?? 'Acceso no autorizado';
+        } else {
+          try {
+            console.log(localStorage.getItem('refresh_token'));
+            const response = await axios.post(`${environment.api}/auth/refresh-token`, {refresh_token: localStorage.getItem('refresh_token')});
+            localStorage.setItem('access_token', response.data.accesToken);
+            localStorage.setItem('refresh_token', response.data.refreshToken);
 
-      }
-    } else if (error.response?.status >= 400 && error.response?.status < 500) {
-      if (error.response?.data?.message) {
-        message = error.response?.data?.message
-      } else if (error.response?.data?.error) {
-        message = error.response?.data?.error
-      }
-      summary = 'Advertencia'
-    } else if (error.response?.status == 300) {
-      // omitir
-    } else {
-      message = error.response.statusText
+            const config = error.config;
+            config.headers.Authorization = `Bearer ${response.data.accessToken}`;
+            return axios.request(config);
+          } catch (error) {
+            localStorage.removeItem('access_token');
+            localStorage.removeItem('refresh_token');
+            localStorage.removeItem('person');
+            window.location.href = '/login';
+            return Promise.reject(error);
+          }
+        }
+        break;
+      default:
+        if (error.response?.status >= 400 && error.response?.status < 500) {
+          if (error.response?.data?.message) {
+            message = error.response?.data?.message;
+          } else if (error.response?.data?.error) {
+            message = error.response?.data?.error;
+          }
+          summary = 'Advertencia';
+        } else {
+          message = error.response.statusText;
+        }
+        break;
     }
 
     if (typeof message === 'object') {
       for (const messageProper in message){
         if (Array.isArray(message[messageProper])) { // Check if messageProper is an array
-          message[messageProper].forEach(function (mensaje) {
+          message[messageProper].forEach(function (msg) {
             setNotification({
               type: type,
               summary: summary,
-              message: mensaje,
+              message: msg,
             });
           });
         }
