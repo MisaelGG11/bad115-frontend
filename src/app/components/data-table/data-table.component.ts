56d752d06aa8e74e66b41a4fc8d829972604ea86
MisaelGG11 <magomez@innovacion.gob.sv>
2024-05-17 17:04:07 -0600

    feat: add data table component (incomplete)

---
+++ src/app/components/data-table/data-table.component.ts
@@ -0,0 +1,89 @@
+import { Component, Input, Output, EventEmitter, TemplateRef, ContentChild } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
+import { CommonModule } from '@angular/common';
+import { PaginatorModule } from 'primeng/paginator';
+
+@Component({
+  selector: 'app-data-table',
+  standalone: true,
+  imports: [TableModule, TagModule, PaginatorModule, CommonModule],
+  templateUrl: './data-table.component.html',
+  styles: ``,
+})
+export class DataTableComponent {
+  @Input() columns: any[] = [];
+  @Input() invertedColors: boolean = false;
+  @Input() rows: any[] = [];
+  @Input() haveActions: boolean = false;
+  @Input() headerAlign: string = 'left';
+  @Input() tableStyle: string = 'min-width: 50rem';
+  @Input() showPagination: boolean = false;
+  @Input() pagination: any = { total: 0, perPage: 10, page: 1 };
+  @ContentChild('actions', { static: true })
+  @Output()
+  setPagination = new EventEmitter();
+  actionsTemplate!: TemplateRef<any>;
+
+  item: any;
+  currentPage: number = 1;
+  rowsToShow: any[] = [];
+
+  ngOnInit() {
+    this.paginate();
+    console.log('DataTableComponent initialized', this.rows);
+  }
+
+  colorTag(estado: any) {
+    if (['Activo'].includes(estado)) {
+      return 'success';
+    }
+    if (['Inactivo'].includes(estado)) {
+      return 'danger';
+    }
+    if (['Inhabilitado'].includes(estado)) {
+      return 'warning';
+    }
+    return 'info';
+  }
+
+  nextPage() {
+    if (this.currentPage * this.pagination.perPage < this.pagination.total) {
+      this.currentPage++;
+    }
+    this.setPagination.emit(this.currentPage);
+    this.paginate();
+    window.scrollTo(0, 0);
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+    this.setPagination.emit(this.currentPage);
+    this.paginate();
+    window.scrollTo(0, 0);
+  }
+
+  goToPage(page: any) {
+    this.currentPage = page;
+    this.setPagination.emit(this.currentPage);
+    this.paginate();
+    window.scrollTo(0, 0);
+  }
+
+  get pageRange() {
+    const total_pages = Math.ceil(this.pagination.total / this.pagination.perPage);
+    const range = 5;
+    let start = this.currentPage - Math.floor(range / 2);
+    start = Math.max(start, 1);
+    let end = start + range - 1;
+    end = Math.min(end, total_pages);
+    start = Math.max(end - range + 1, 1);
+    return Array.from({ length: end - start + 1 }, (_, i) => start + i);
+  }
+
+  paginate() {
+    this.rowsToShow = this.rows;
+  }
+}
--- src/app/views/dashboard/home/home.component.ts
+++ src/app/views/dashboard/home/home.component.ts
@@ -1,22 +1,117 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { toast } from 'ngx-sonner';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DataTableComponent } from '../../../components/data-table/data-table.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { CalendarModule } from 'primeng/calendar';
-
+import { CommonModule } from '@angular/common';
+import { TooltipModule } from 'primeng/tooltip';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [InputTextModule, ButtonModule, FormsModule, ReactiveFormsModule, CalendarModule],
+  imports: [
+    InputTextModule,
+    ButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CalendarModule,
+    DataTableComponent,
+    TooltipModule,
+    CommonModule,
+  ],
   templateUrl: './home.component.html',
   styles: ``,
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   protected readonly toast = toast;
 
   value: string = '';
   date2: string = '';
+
+  rows: any[] = [];
+
+  columnas = [
+    { field: 'id', header: 'ID', column_align: 'left', row_align: 'center' },
+    { field: 'nombre', header: 'Nombre', column_align: 'left', row_align: 'center' },
+    { field: 'apellido', header: 'Apellido', column_align: 'left', row_align: 'center' },
+    { field: 'estado', header: 'Estado', column_align: 'center', row_align: 'center' },
+  ];
+
+  empleados: any = [
+    { id: 1, nombre: 'Juan', apellido: 'Pérez', estado: { id: 1, nombre: 'Activo' } },
+    { id: 2, nombre: 'María', apellido: 'Gómez', estado: { id: 2, nombre: 'Inactivo' } },
+    { id: 3, nombre: 'Pedro', apellido: 'Martínez', estado: { id: 1, nombre: 'Activo' } },
+    { id: 4, nombre: 'Ana', apellido: 'López', estado: { id: 3, nombre: 'Inhabilitado' } },
+    { id: 5, nombre: 'Luis', apellido: 'Hernández', estado: { id: 1, nombre: 'Activo' } },
+    { id: 6, nombre: 'Sara', apellido: 'García', estado: { id: 2, nombre: 'Inactivo' } },
+    { id: 7, nombre: 'Carlos', apellido: 'Ruiz', estado: { id: 1, nombre: 'Activo' } },
+    { id: 9, nombre: 'Laura', apellido: 'Fernández', estado: { id: 3, nombre: 'Inhabilitado' } },
+    { id: 10, nombre: 'Juan', apellido: 'Pérez', estado: { id: 1, nombre: 'Activo' } },
+    { id: 11, nombre: 'Juan', apellido: 'Pérez', estado: { id: 1, nombre: 'Activo' } },
+    { id: 12, nombre: 'María', apellido: 'Gómez', estado: { id: 2, nombre: 'Inactivo' } },
+    { id: 13, nombre: 'Pedro', apellido: 'Martínez', estado: { id: 1, nombre: 'Activo' } },
+    { id: 14, nombre: 'Ana', apellido: 'López', estado: { id: 3, nombre: 'Inhabilitado' } },
+    { id: 15, nombre: 'Luis', apellido: 'Hernández', estado: { id: 1, nombre: 'Activo' } },
+    { id: 16, nombre: 'Sara', apellido: 'García', estado: { id: 2, nombre: 'Inactivo' } },
+    { id: 17, nombre: 'Carlos', apellido: 'Ruiz', estado: { id: 1, nombre: 'Activo' } },
+    { id: 18, nombre: 'Laura', apellido: 'Fernández', estado: { id: 3, nombre: 'Inhabilitado' } },
+    { id: 19, nombre: 'María', apellido: 'Gómez', estado: { id: 2, nombre: 'Inactivo' } },
+    { id: 21, nombre: 'Pedro', apellido: 'Martínez', estado: { id: 1, nombre: 'Activo' } },
+    { id: 22, nombre: 'Ana', apellido: 'López', estado: { id: 3, nombre: 'Inhabilitado' } },
+    { id: 23, nombre: 'Luis', apellido: 'Hernández', estado: { id: 1, nombre: 'Activo' } },
+    { id: 24, nombre: 'Sara', apellido: 'García', estado: { id: 2, nombre: 'Inactivo' } },
+    { id: 25, nombre: 'Carlos', apellido: 'Ruiz', estado: { id: 1, nombre: 'Activo' } },
+    { id: 26, nombre: 'Laura', apellido: 'Fernández', estado: { id: 3, nombre: 'Inhabilitado' } },
+  ];
+  pagination = {
+    total: this.rows.length,
+    perPage: 10,
+    page: 1,
+  };
+
+  getData = (): void => {
+    const startIndex = (this.pagination.page - 1) * this.pagination.perPage;
+    const endIndex = startIndex + this.pagination.perPage;
+    this.rows = this.empleados.slice(startIndex, endIndex);
+  };
+
+  acciones = [
+    {
+      label: 'Visualizar',
+      icon: 'visibility',
+      permiso: true,
+      onClick: (value: any) => {
+        console.log('Visualizar:', value);
+      },
+    },
+    {
+      label: 'Editar',
+      icon: 'edit',
+      permiso: true,
+      onClick: (value: any) => {
+        console.log('Editar:', value);
+      },
+    },
+    {
+      label: 'Eliminar',
+      icon: 'delete',
+      permiso: false,
+      onClick: (value: any) => {
+        console.log('Eliminar:', value);
+      },
+    },
+  ];
+
+  paginatePage(page: any) {
+    this.pagination.page = page;
+    console.log('Paginate page:', page);
+    this.getData();
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.getData();
+  }
 }
