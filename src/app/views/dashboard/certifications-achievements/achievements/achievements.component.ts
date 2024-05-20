3430d895f930557572421ea19eea4c66dbdc736a
MisaelGG11 <90297880+MisaelGG11@users.noreply.github.com>
2024-05-20 07:07:32 -0600

    feat: add initial achievements components

---
+++ src/app/views/dashboard/certifications-achievements/achievements/achievements.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-achievements',
+  standalone: true,
+  imports: [],
+  templateUrl: './achievements.component.html',
+  styles: ``
+})
+export class AchievementsComponent {
+
+}
