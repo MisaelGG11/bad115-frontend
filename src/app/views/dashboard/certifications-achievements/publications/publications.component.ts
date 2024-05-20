09212f198f585df1958dabf3bd40ea1d5717b4fa
MisaelGG11 <90297880+MisaelGG11@users.noreply.github.com>
2024-05-20 07:07:58 -0600

    feat: add initial publications components

---
+++ src/app/views/dashboard/certifications-achievements/publications/publications.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-publications',
+  standalone: true,
+  imports: [],
+  templateUrl: './publications.component.html',
+  styles: ``
+})
+export class PublicationsComponent {
+
+}
