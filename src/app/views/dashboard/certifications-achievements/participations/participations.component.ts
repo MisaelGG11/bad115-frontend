aaab9c3fc22dff073f96929b69953511696fe29b
MisaelGG11 <90297880+MisaelGG11@users.noreply.github.com>
2024-05-20 07:08:16 -0600

    feat: add initial participations components

---
+++ src/app/views/dashboard/certifications-achievements/participations/participations.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-participations',
+  standalone: true,
+  imports: [],
+  templateUrl: './participations.component.html',
+  styles: ``
+})
+export class ParticipationsComponent {
+
+}
