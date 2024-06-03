63cf7a181f83810745c3f8c4d0a83075b901e2de
MisaelGG11 <magomez@innovacion.gob.sv>
2024-06-02 18:43:39 -0600

    feat: manage permission to create roles and permissions

--- src/app/views/dashboard/permission-management/permission-management.component.ts
+++ src/app/views/dashboard/permission-management/permission-management.component.ts
@@ -1,5 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { GlobalFunctionsService } from '../../../utils/services/global-functions.service';
+import { PERMISSIONS } from '../../../utils/constants.utils';
 import { PermissionListComponent } from './components/permission-list/permission-list.component';
 import { CreatePermissionComponent } from './components/create-permission/create-permission.component';
 
@@ -11,9 +13,14 @@ import { CreatePermissionComponent } from './components/create-permission/create
   styles: [],
 })
 export class PermissionManagementComponent {
+  private global = inject(GlobalFunctionsService);
   showAddModal = signal(false);
 
   onClickCreate() {
     this.showAddModal.set(true);
+  }
+
+  canCreate() {
+    return this.global.verifyPermission(PERMISSIONS.CREATE_USER);
   }
 }
--- src/app/views/dashboard/role-management/role-management.component.ts
+++ src/app/views/dashboard/role-management/role-management.component.ts
@@ -1,7 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { RolesListComponent } from './components/role-list/role-list.component';
 import { CreateRoleComponent } from './components/create-role/create-role.component';
+import { GlobalFunctionsService } from '../../../utils/services/global-functions.service';
+import { PERMISSIONS } from '../../../utils/constants.utils';
 
 @Component({
   selector: 'app-role-management-type',
@@ -11,9 +13,14 @@ import { CreateRoleComponent } from './components/create-role/create-role.compon
   styles: [],
 })
 export class RoleManagementComponent {
+  private global = inject(GlobalFunctionsService);
   showAddModal = signal(false);
 
   onClickCreate() {
     this.showAddModal.set(true);
+  }
+
+  canCreate() {
+    return this.global.verifyPermission(PERMISSIONS.CREATE_ROLE);
   }
 }
