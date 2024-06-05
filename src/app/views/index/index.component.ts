fe0832264a01bec4c367822d4f57ea8e915720ca
MisaelGG11 <gg20031@ues.edu.sv>
2024-06-05 13:26:45 -0600

    style: refactor styles for index view

--- src/app/views/index/index.component.ts
+++ src/app/views/index/index.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { IndexNavbarComponent } from '../../components/navbars/index-navbar/index-navbar.component';
 import { FooterComponent } from '../../components/footers/footer/footer.component';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [RouterLink, IndexNavbarComponent, FooterComponent],
+  imports: [RouterLink, IndexNavbarComponent, FooterComponent, DividerModule],
   templateUrl: './index.component.html',
   styles: ``,
 })
